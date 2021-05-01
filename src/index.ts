886122e7d38e3149af805387bad6e7fd5390a864
Arash <arash.shakery@gmail.com>
2021-05-01 23:35:41 +0430

    throttled auto-disconnet

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
 import bridge from './bridge';
 
 let activeConnections = 0;
+let scheduled: NodeJS.Timeout;
+let disconnecting: Promise<void> | null = null;
+function scheduleDisconnect() {
+  console.log('scheduling disconnet');
+
+  clearTimeout(scheduled);
+  scheduled = setTimeout(() => {
+    if (activeConnections) return;
+
+    disconnecting = bridge.disconnect(); // never rejects
+    disconnecting.then(() => {
+      disconnecting = null;
+    });
+  }, 3000);
+}
+
 function wrapConn<F>(fn: F): F {
   return (async function () {
+    clearTimeout(scheduled);
+    if (disconnecting) {
+      try {
+        await disconnecting;
+      } catch (e) {}
+    }
+
     try {
       activeConnections++;
       await bridge.connect();
@@ -10,7 +33,7 @@ function wrapConn<F>(fn: F): F {
     } finally {
       activeConnections--;
       if (!activeConnections) {
-        bridge.disconnect(); // never rejects
+        scheduleDisconnect();
       }
     }
   } as any) as F;
